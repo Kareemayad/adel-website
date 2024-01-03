d00a5968f0f55b853f499b93971af02b99bfb426
Kareemayad <kareemayad@gmail.com>
2024-01-03 17:07:36 +0200

    Update footer and logo components

--- src/lib/components/layout/footer.svelte
+++ src/lib/components/layout/footer.svelte
@@ -25,30 +25,4 @@
 
 <footer
 	class="flex flex-col justify-between w-full h-full gap-8 px-10 py-4 border-t sm:gap-0 sm:flex-row bg-base-200 text-base-content border-base-300"
->
-	<div class="flex flex-col items-center sm:flex-row">
-		<a href="/">
-			<Logo />
-		</a>
-
-		{#if quote}
-			<div
-				transition:fade={{ delay: 250, duration: 300 }}
-				class="flex flex-col gap-2 text-center sm:gap-0"
-			>
-				<x>"{quote?.text}"</x>
-				<x class="flex flex-col sm:flex-row"
-					><b>- {quote?.quoteAuthor}</b><i class="opacity-70"
-						><x class="hidden sm:inline">, </x><x>{quote?.apiAuthor}</x></i
-					></x
-				>
-			</div>
-		{/if}
-	</div>
-	<div class="flex justify-center w-full sm:w-auto">
-		<div class="flex gap-4 text-3xl">
-			<!-- svelte-ignore a11y-missing-content -->
-			<a href="https://github.com/MahmoodKhalil57/svelteTemplateV1" class="i-mdi-github" />
-		</div>
-	</div>
-</footer>
+/>
--- src/routes/(app)/(index)/+page.svelte
+++ src/routes/(app)/(index)/+page.svelte
@@ -1,1 +1,29 @@
-<div class="text-6xl">Adel me3aras kebir</div>
+<div class="flex flex-col bg-fuchsia-500 h-full w-screen">
+	<div class="mt-0">
+		<img src="Screenshot 2024-01-02 at 10.46 1.png" alt="party" />
+	</div>
+	<div class="text-amber-300 text-xl font-bold ml-2">Event Title</div>
+
+	<div class="bg-slate-200 text-sky-600 w-80 m-3 p-4">
+		<div>
+			Description of event <br />🔥🔥🔥*DPP EVENTS*🔥🔥🔥 <br /> presents to you <br /> * THE MAIN
+			EVENT * <br />📍FELLINI 🕛 12 NOVEMBER FROM 8:00 TILL 12:00 DOORS CLOSE AT 10:00 <br /> WE'RE
+			BACK <br />we would like to invite you all to our biggest and best party yet❤️‍🔥❤️‍🔥<br /> you
+			just finished your exams and resits we think its time for a well deserved break <br />
+			For all of the people suffering from FOMO you won’t want to miss out on this one imagine a party
+			with -DJ Pinguin 🎧 -DJ Wakil 🎧 -DJ YH Studios 🎧 -A PYROTECHNICS DJ 🎧 -3 separate bars with
+			20 bartenders so no waiting in line🍾🍾🍾 **VIP TABLES** will be on a terrace overlooking the whole
+			venue with a bottle on the house for each table and no waiting on door for entering with their
+			own separate bar. VIP tables will not be available for anyone if you want to purchase a table please
+			WhatsApp +31627534979 There will also be **VIP LOUNGES** they will not be available for anyone
+			if you want to purchase a table please WhatsApp +31627534979 ⚠️CORONA CHECK COVID QR Code required⚠️
+			Follow us on Instagram to get a chance and win 100 euros @dppevents 🎟 tickets are available at
+			www.dppevents.net With all of the covid measures going on now you will need to provide us with
+			a rapid test no longer than 24 hours before the event that you can do for free from this link
+			<br />https://afspraak.testenvoortoegang.org
+			<br />/afspraakbevestiging/8352e2e7-ce99-4b7b-3cf3-08d96de6fbf0/TATCARRXYW or a vaccine
+			certificate using the corona health check app https://coronacheck.nl/nl/. For any inquiries
+			please text this number via WhatsApp +31627534979
+		</div>
+	</div>
+</div>
